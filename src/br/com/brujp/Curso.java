c1178214b2db0e726fc5a2d16471cbf29ecc728c
Bruno Pinto <bruno.jpinto@outlook.com>
2023-07-25 11:39:24 -0300

    Mais práticas com relacionamentos

--- src/br/com/brujp/Curso.java
+++ src/br/com/brujp/Curso.java
@@ -30,4 +30,19 @@ public class Curso {
     public void adiciona(Aula aula) {
         this.aulas.add(aula);
     }
+
+    public int getTempoTotal() {
+        int tempoTotal = 0;
+        for (Aula aula : aulas) {
+            tempoTotal += aula.getTempo();
+        }
+        return tempoTotal;
+    }
+
+    @Override
+    public String toString() {
+        return "[Curso: " + nomeCurso +
+                ". Tempo total: " + this.getTempoTotal() +
+                " minutos. Aulas: " + this.aulas + "]";
+    }
 }
---
+++ src/br/com/brujp/TestaCurso2.java
@@ -0,0 +1,33 @@
+package br.com.brujp;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TestaCurso2 {
+
+    public static void main(String[] args) {
+
+        Curso javaColecoes = new Curso(
+                "Dominando as coleções do Java",
+                "Bruno Pinto");
+
+        javaColecoes.adiciona(new Aula("Uau 1", 15));
+        javaColecoes.adiciona(new Aula("Uau 2", 45));
+        javaColecoes.adiciona(new Aula("Uau 3", 23));
+
+        List<Aula> aulasImutaveis = javaColecoes.getAulas();
+        System.out.println("Aulas imutáveis: " + aulasImutaveis);
+
+        //Ordenando a lista
+        List<Aula> aulas = new ArrayList<>(aulasImutaveis);
+
+        Collections.sort(aulas);
+        System.out.println(aulas);
+
+        System.out.println(javaColecoes.getTempoTotal());
+
+        System.out.println(javaColecoes);
+        
+    }
+}
